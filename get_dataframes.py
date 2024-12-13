003ffc84e03463692fb0ea28930cd9a064afef90
flipzky1337 <rorytheloli1337@gmail.com>
2024-12-13 16:45:43 +0500

    remove reduntant flag in dataframes class

--- get_dataframes.py
+++ get_dataframes.py
@@ -1,25 +1,21 @@
 import kagglehub
+import pathlib
+import os
 import pandas as pd
 
 
 class DataFrames:
-    def __init__(self, flag_is_data_downloaded = False):
-        self.flag_is_data_downloaded = flag_is_data_downloaded
+    def __init__(self):
 
-        if self.flag_is_data_downloaded:
-
-            path_flights = r'C:\Users\Mi\.cache\kagglehub\datasets\patrickzel\flight-delay-and-cancellation-dataset-2019-2023\versions\7\flights_sample_3m.csv'
-            path_with_tail_number = r'C:\Users\Mi\.cache\kagglehub\datasets\deepankurk\flight-take-off-data-jfk-airport\versions\1\M1_final.csv'
+        if os.path.isfile(fr'{pathlib.Path.home()}\.cache\kagglehub\datasets\patrickzel\flight-delay-and-cancellation-dataset-2019-2023\versions\7\flights_sample_3m.csv') and os.path.isfile(fr'{pathlib.Path.home()}\.cache\kagglehub\datasets\deepankurk\flight-take-off-data-jfk-airport\versions\1\M1_final.csv'):
+            path_flights = fr'{pathlib.Path.home()}\.cache\kagglehub\datasets\patrickzel\flight-delay-and-cancellation-dataset-2019-2023\versions\7\flights_sample_3m.csv'
+            path_with_tail_number = fr'{pathlib.Path.home()}\.cache\kagglehub\datasets\deepankurk\flight-take-off-data-jfk-airport\versions\1\M1_final.csv'
 
             self.df_flights = pd.read_csv(path_flights)
             self.df_with_tail_number = pd.read_csv(path_with_tail_number)
-
         else:
-
             path_flights = kagglehub.dataset_download("patrickzel/flight-delay-and-cancellation-dataset-2019-2023")
             path_with_tail_number = kagglehub.dataset_download("deepankurk/flight-take-off-data-jfk-airport")
-
-            self.flag_is_data_downloaded = True
 
             self.df_flights = pd.read_csv(path_flights)
             self.df_with_tail_number = pd.read_csv(path_with_tail_number)
